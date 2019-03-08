e6bec7facad1f47ffb64c95e4f40300f3b1492db
Your Name <Hugo Rosas>
2019-03-08 16:14:11 -0600

    guardar

--- src/App.js
+++ src/App.js
@@ -2,24 +2,84 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {todos} from './todos.json';
+import TodoForm from './components/TodoForm';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos
+    }
+    this.handleAddTodo = this.handleAddTodo.bind(this);
+  }
+
+  handleAddTodo(todo) {
+    this.setState({
+      todos: [...this.state.todos, todo]
+    })
+  }
+
+  removeTodo(index) {
+    this.setState({
+      todos: this.state.todos.filter((e, i) => {
+        return i !== index
+      })
+    })
+  }
+
   render() {
+    const todos = this.state.todos.map((todos, i) => {
+      return (
+        <div className="col-md-4" key={i}>
+          <div className="card mt-4">
+            <div className="card-header">
+              <h3>{todos.title}</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+                {todos.priority}
+              </span>
+            </div>
+            <div className="card-body">
+              <p>{todos.description}</p>
+              <p><mark>{todos.responsible}</mark></p>
+            </div>
+            <div className="card-footer">
+              <button
+                className="btn btn-danger"
+                onClick={this.removeTodo.bind(this, i)}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      )
+    })
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+        <nav className="navbar navbar-dark bg-dark">
+          <a href="" className="text-white">
+            Task
+            <span className="badge badge-pill badge-light ml-2">
+              {this.state.todos.length}
+            </span>
           </a>
-        </header>
+        </nav>
+        <div className="container">
+          <div className="row mt-4">
+            <div className="col-md-3">
+              <img src={logo} className="App-logo" alt="logo" />
+              <TodoForm onAddTodo={this.handleAddTodo}/>
+            </div>
+            <div className="col-md-9">
+              <div className="row">
+                {todos}
+              </div>
+            </div>
+          </div>
+        </div>
+        
       </div>
     );
   }
